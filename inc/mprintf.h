021a51f907f4cd9df219a9c2e053e5d270569ddf
ratman528 <adevries528@gmail.com>
2024-05-12 20:34:54 -0500

    added header guards, modified .gitignore

--- inc/mprintf.h
+++ inc/mprintf.h
@@ -1,4 +1,9 @@
 
+// header guard
+#ifndef __MPRINTF_H
+#define __MPRINTF_H
+
+
 #include <stdarg.h>
 #include <stdint.h>
 
@@ -8,3 +13,5 @@ int32_t snprintf_(char * restrict out_str, uint32_t buf_len, const char * restri
 int32_t vsnprintf_(char * restrict out_str, uint32_t buf_len, const char * restrict format_str, va_list arg);
 char * strncpy_(char * restrict dest_str, const char * restrict src_str, uint32_t len);
 uint32_t strlen_(const char * restrict str);
+
+#endif // __MPRINTF_H
